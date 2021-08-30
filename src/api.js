bb32b540ca4dde7db4cb5a206fcb9ce41f9fec63
goumi <goumi1009@gmail.com>
2021-08-30 16:28:53 +0900

    페이지목록을 위한 컴포넌트들 추가

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const request = async (url, options = {}) => {
+  try {
+    const res = await fetch(`${API_END_POINT}${url}`, options);
+    if (res.ok) {
+      const json = await res.json();
+      return json;
+    }
+    throw new Error('API 호출 오류');
+  } catch (e) {
+    alert(e.message);
+  }
+};
