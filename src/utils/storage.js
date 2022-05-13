0c1c68d869d829342e7a3d642ecea5d72781ae16
goumi1009 <goumi1009@gmail.com>
2022-05-13 11:37:17 +0900

    chore: 세미콜론 추가

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,15 +1,14 @@
-const storage = window.localStorage
+const storage = window.localStorage;
 
 export const getItem = (key, defaultValue) => {
-  const storedValue = JSON.parse(storage.getItem(key)) || defaultValue
-  return storedValue
-}
+  const storedValue = JSON.parse(storage.getItem(key)) || defaultValue;
+  return storedValue;
+};
 
 export const setItem = (key, value) => {
-  console.log(key, value)
-  storage.setItem(key, JSON.stringify(value))
-}
+  storage.setItem(key, JSON.stringify(value));
+};
 
 export const removeItem = (key) => {
-  storage.removeItem(key)
-}
+  storage.removeItem(key);
+};
