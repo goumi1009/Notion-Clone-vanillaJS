a4edfd18946b7473d852b1dcb7df81e36db5a574
goumi1009 <goumi1009@gmail.com>
2022-05-16 00:03:55 +0900

    refactor: editorContainer state APP에서 전해주도록 변경

--- src/App.js
+++ src/App.js
@@ -1,86 +1,154 @@
-import SideContainer from './SideContainer.js'
-import EditorContainer from './EditorContainer.js'
-import { initRouter } from './router.js'
-import { request } from './api.js'
-import { push } from './router.js'
+import SideContainer from './SideContainer.js';
+import EditorContainer from './EditorContainer.js';
+import { initRouter } from './router.js';
+import { request } from './api.js';
+import { push } from './router.js';
+import { getItem, setItem } from './storage.js';
 
 export default function App({ $target }) {
   this.state = {
     user: {
       name: 'GOUM',
-      img: '/src/assets/img-profile-default.svg'
+      img: '/src/assets/img-profile-default.svg',
     },
-    pages: []
-  }
+    pages: [],
+    currentPageId: null,
+    currentPage: {},
+  };
 
-  this.setState = (nextState) => {
-    this.state = nextState
-    sideConatiner.setState(this.state)
-  }
+  this.setState = async (nextState) => {
+    if (this.state.currentPageId !== nextState.currentPageId) {
+      this.state = nextState;
+      autoSaveLocalKey = `autoSaveData_${this.state.currentPageId}`;
+      await fetchPost();
+      return;
+    }
+    this.state = nextState;
+    sideConatiner.setState(this.state);
+    editorContainer.setState({
+      id: this.state.currentPageId,
+      page: this.state.currentPage,
+    });
+  };
 
   const sideConatiner = new SideContainer({
     $target,
     initialState: this.state,
     onDeletePage: async (id) => {
       const pageList = await request(`/${id}`, {
-        method: 'DELETE'
-      })
-      await init()
+        method: 'DELETE',
+      });
+      await init();
       this.setState({
         ...this.state,
-        pageList
-      })
-      push(`/`)
+        pageList,
+      });
+      push(`/`);
     },
     onAddPage: (id = null) => {
-      addPage('제목없음', id)
-    }
-  })
+      addPage('제목없음', id);
+    },
+  });
 
   const addPage = async (title, parent) => {
     const pageList = await request('', {
       method: 'POST',
       body: JSON.stringify({
         title,
-        parent
-      })
-    })
-    push(`/documents/${pageList.id}`)
-    await init()
+        parent,
+      }),
+    });
+    push(`/documents/${pageList.id}`);
+    await init();
     this.setState({
       ...this.state,
-      pageList
-    })
-  }
+      pageList,
+    });
+  };
+
+  let autoSaveLocalKey = `autoSaveData_${this.state.currentPageId}`;
+  let timer = null;
 
   const editorContainer = new EditorContainer({
     $target,
     initialState: {
-      id: null
+      id: this.state.currentPageId,
+      page: this.state.currentPage,
+    },
+    autoSave: (page) => {
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(async () => {
+        setItem(autoSaveLocalKey, {
+          ...page,
+          saveDate: new Date(),
+        });
+
+        await request(`/${this.state.currentPageId}`, {
+          method: 'PUT',
+          body: JSON.stringify(page),
+        });
+      }, 2000);
+    },
+  });
+
+  this.setState({
+    ...this.state,
+    currentPage: getItem(autoSaveLocalKey, {
+      title: '',
+      content: '',
+    }),
+  });
+
+  const fetchPost = async () => {
+    const { currentPageId } = this.state;
+    const currentPage = await request(`/${currentPageId}`);
+
+    const autoSavePage = getItem(autoSaveLocalKey, {
+      title: '',
+      content: '',
+    });
+    const { saveDate } = autoSavePage;
+    if (saveDate && saveDate > currentPage.updatedAt) {
+      if (confirm('저장되지 않은 데이터가 있습니다. 불러올까요?')) {
+        this.setState({
+          ...this.state,
+          currentPage: autoSavePage,
+        });
+        return;
+      }
     }
-  })
+    this.setState({
+      ...this.state,
+      currentPage,
+    });
+  };
 
   const init = async () => {
     const pages = await request('', {
       method: 'GET',
-    })
+    });
     this.setState({
       ...this.state,
-      pages
-    })
-    console.log(this.state)
-  }
-  init()
+      pages,
+    });
+  };
+  init();
 
   this.route = () => {
-    const { pathname } = window.location
+    const { pathname } = window.location;
     if (pathname.indexOf('/documents/') === 0) {
-      const [, , id] = pathname.split('/')
-      editorContainer.setState({ id })
+      const [, , id] = pathname.split('/');
+      this.setState({
+        ...this.state,
+        currentPageId: id,
+      });
     }
-  }
+  };
 
-  this.route()
+  this.route();
 
-  initRouter(() => this.route())
+  initRouter(() => this.route());
 }
--- src/EditorContainer.js
+++ src/EditorContainer.js
@@ -1,91 +1,35 @@
-import { request } from './api.js'
-import { getItem, setItem } from './storage.js'
-import Editor from './Editor.js'
+import Editor from './Editor.js';
 
-export default function EditorContainer({ $target, initialState }) {
+export default function EditorContainer({ $target, initialState, autoSave }) {
+  const $editorContainer = document.createElement('div');
+  $editorContainer.classList.add('editor-container');
 
-  const $editorContainer = document.createElement('div')
-  $editorContainer.classList.add('editor-container')
+  this.state = initialState;
 
-  this.state = initialState
-
-  let autoSaveLocalKey = `autoSaveData_${this.state.id}`
-
-  const page = getItem(autoSaveLocalKey, {
-    title: '',
-    content: ''
-  })
-
-  let timer = null
+  let timer = null;
 
   const editor = new Editor({
     $target: $editorContainer,
-    initialState: page,
-    autoSave: (page) => {
-      if (timer !== null) {
-        clearTimeout(timer)
+    initialState: this.state.page,
+    autoSave,
+  });
+
+  this.setState = async (nextState) => {
+    this.state = nextState;
+    this.render();
+    editor.setState(
+      this.state.page || {
+        title: '',
+        content: '',
       }
-
-      timer = setTimeout(async () => {
-        console.log(page)
-        setItem(autoSaveLocalKey, {
-          ...page,
-          saveDate: new Date()
-        })
-
-        await request(`/${this.state.id}`, {
-          method: 'PUT',
-          body: JSON.stringify(page)
-        })
-      }, 2000)
-    }
-  })
-
-  this.setState = async nextState => {
-    if (this.state.id !== nextState.id) {
-      autoSaveLocalKey = `autoSaveData_${nextState.id}`
-      this.state = nextState
-      await fetchPost()
-      return
-    }
-
-    this.state = nextState
-    this.render()
-    editor.setState(this.state.page || {
-      title: '',
-      content: ''
-    })
-  }
+    );
+  };
 
   this.render = () => {
     if (this.state.id !== null) {
-      $target.appendChild($editorContainer)
+      $target.appendChild($editorContainer);
     }
-  }
+  };
 
-  this.render()
-
-  const fetchPost = async () => {
-    const { id } = this.state
-    const page = await request(`/${id}`)
-
-    const autoSavePage = getItem(autoSaveLocalKey, {
-      title: '',
-      content: ''
-    })
-    const { saveDate } = autoSavePage
-    if (saveDate && saveDate > page.updatedAt) {
-      if (confirm('저장되지 않은 데이터가 있습니다. 불러올까요?')) {
-        this.setState({
-          ...this.state,
-          page: autoSavePage
-        })
-        return
-      }
-    }
-    this.setState({
-      ...this.state,
-      page
-    })
-  }
+  this.render();
 }
